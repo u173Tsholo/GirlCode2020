f56f96ebf78e8e3d2b9b063e35aa00ba464e8aa2
u173Tsholo <u17391718@tuks.co.za>
2020-07-31 23:11:45 +0200

    login/reg functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,25 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//component modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+//import { MatMenuModule } from '@angular/material/menu';
+//import { MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatTableModule} from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+//import { MatGridListModule } from '@angular/material/grid-list'
+//import { MatRadioModule } from '@angular/material/radio';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -20,7 +39,20 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    //MatMenuModule,
+    //MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatTableModule,
+    HttpClientModule,
+    FormsModule,
+    MatInputModule, MatListModule,
+    MatCardModule, MatFormFieldModule, MatDialogModule, ReactiveFormsModule
+    //MatGridListModule, MatRadioModule, ,
   ],
   providers: [],
   bootstrap: [AppComponent]
